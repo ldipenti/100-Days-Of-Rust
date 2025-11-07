808b2846d43cd5a5adbe99b5d3ef0c99b398ad52
Lucas Di Pentima <lucas@di-pentima.com.ar>
2025-11-07 10:23:38 -0300

    Simplified code, added negative test

--- Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
+++ Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
@@ -3,23 +3,17 @@ fn main() {
     println!("{}", where_is_nemo("I am finding Nemo !".to_string()));
     println!("{}", where_is_nemo("Nemo is me".to_string()));
     println!("{}", where_is_nemo("I Nemo am".to_string()));
+    println!("{}", where_is_nemo("I am Doris , and you ?".to_string()));
 }
 
 fn where_is_nemo(sentence: String) -> String {
     let nemo = "Nemo";
     let mut pos = 0;
-    let mut nemo_pos = pos;
     for word in sentence.split(' ') {
-        if word == nemo {
-            nemo_pos = pos+1;
-            break;
-        }
         pos += 1;
+        if word == nemo {
+            return format!("I found Nemo at {}!", pos).to_string();
+        }
     }
-
-    if nemo_pos > 0 {
-        return format!("I found Nemo at {}!", nemo_pos).to_string();
-    } else {
-        return "I can't find Nemo :(".to_string();
-    }
+    return "I can't find Nemo :(".to_string();
 }
