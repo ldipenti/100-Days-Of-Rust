ff2149cee0c636e19948afc26510dd2a80847d6c
Lucas Di Pentima <lucas@di-pentima.com.ar>
2025-11-07 10:20:02 -0300

    Fixed Finding Nemo exercise by returning the word position

--- Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
+++ Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
@@ -1,16 +1,25 @@
 
 fn main() {
     println!("{}", where_is_nemo("I am finding Nemo !".to_string()));
+    println!("{}", where_is_nemo("Nemo is me".to_string()));
+    println!("{}", where_is_nemo("I Nemo am".to_string()));
 }
 
 fn where_is_nemo(sentence: String) -> String {
-    let nemo = "Nemo ";
-    match sentence.as_str().rfind(nemo) {
-        Some(val) => {
-            return format!("I found Nemo at {}", val+1).to_string();
-        },
-        None => {
-            return "I can't find Nemo :(".to_string();
+    let nemo = "Nemo";
+    let mut pos = 0;
+    let mut nemo_pos = pos;
+    for word in sentence.split(' ') {
+        if word == nemo {
+            nemo_pos = pos+1;
+            break;
         }
+        pos += 1;
+    }
+
+    if nemo_pos > 0 {
+        return format!("I found Nemo at {}!", nemo_pos).to_string();
+    } else {
+        return "I can't find Nemo :(".to_string();
     }
 }
