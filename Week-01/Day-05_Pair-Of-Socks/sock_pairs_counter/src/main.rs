c0f98659d41f36465651171940c84163f4fcc1f1
Lucas Di Pentima <lucas@di-pentima.com.ar>
2025-11-12 11:01:45 -0300

    Day 5 complete - Joseph now knows how much pairs he has.

--- Week-01/Day-05_Pair-Of-Socks/sock_pairs_counter/src/main.rs
+++ Week-01/Day-05_Pair-Of-Socks/sock_pairs_counter/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let socks = [
         "AA",
@@ -11,6 +13,14 @@ fn main() {
     }
 }
 
-fn sock_pairs(_sock_set: &str) -> u32 {
-    0
+fn sock_pairs(sock_set: &str) -> u32 {
+    let mut sock_kinds = HashMap::new();
+    let mut sock_pairs = 0;
+    for sock in sock_set.as_bytes().iter() {
+        sock_kinds.insert(sock, sock_kinds.get(sock).unwrap_or(&0)+1);
+    }
+    for sock_kind in sock_kinds.keys() {
+        sock_pairs += sock_kinds.get(sock_kind).unwrap() / 2;
+    }
+    sock_pairs
 }
