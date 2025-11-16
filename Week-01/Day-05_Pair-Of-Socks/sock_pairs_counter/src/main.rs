ac97c5f867aae88b55484e8b4e578ba3d66308f3
Lucas Di Pentima <lucas@di-pentima.com.ar>
2025-11-16 11:55:27 -0300

    Trying cargo fmt

--- Week-01/Day-05_Pair-Of-Socks/sock_pairs_counter/src/main.rs
+++ Week-01/Day-05_Pair-Of-Socks/sock_pairs_counter/src/main.rs
@@ -1,15 +1,14 @@
 use std::collections::HashMap;
 
 fn main() {
-    let socks = [
-        "AA",
-        "ABABC",
-        "CABBACCC",
-        ""
-    ];
+    let socks = ["AA", "ABABC", "CABBACCC", ""];
 
     for (test_nr, sock_set) in socks.iter().enumerate() {
-        println!("Sock set #{} has {} sock pairs", test_nr, sock_pairs(sock_set));
+        println!(
+            "Sock set #{} has {} sock pairs",
+            test_nr,
+            sock_pairs(sock_set)
+        );
     }
 }
 
@@ -17,7 +16,7 @@ fn sock_pairs(sock_set: &str) -> u32 {
     let mut sock_kinds = HashMap::new();
     let mut sock_pairs = 0;
     for sock in sock_set.as_bytes().iter() {
-        sock_kinds.insert(sock, sock_kinds.get(sock).unwrap_or(&0)+1);
+        sock_kinds.insert(sock, sock_kinds.get(sock).unwrap_or(&0) + 1);
     }
     for sock_kind in sock_kinds.keys() {
         sock_pairs += sock_kinds.get(sock_kind).unwrap() / 2;
