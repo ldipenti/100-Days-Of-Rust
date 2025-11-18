11c22f4be6a14585d2c900818d0c612543a3aa29
Lucas Di Pentima <lucas@di-pentima.com.ar>
2025-11-17 22:01:41 -0300

    Day 6 complete.

--- Week-01/Day-06_Next-Prime/next_prime/src/main.rs
+++ Week-01/Day-06_Next-Prime/next_prime/src/main.rs
@@ -1,11 +1,43 @@
 fn main() {
-    let test_cases = [12, 24, 11];
+    let test_cases = [1, 2, 12, 24, 11, 123456];
 
     for (test_nr, test_case) in test_cases.iter().enumerate() {
-        println!("Test #{}, next prime of {} is {}", test_nr, test_case, next_prime(*test_case));
+        println!(
+            "Test #{}, next prime of {} is {}",
+            test_nr,
+            test_case,
+            next_prime(*test_case)
+        );
     }
 }
 
-fn next_prime(_num: i32) -> i32 {
+fn next_prime(num: u32) -> u32 {
+    for n in num..u32::MAX {
+        // 2 is even and prime
+        if n == 2 {
+            return n;
+        }
+        // other even numbers are not primes
+        if n > 2 && n % 2 == 0 {
+            continue;
+        }
+        let mut is_prime = false;
+        for factor in 3..n + 1 {
+            // skip even factors
+            if factor % 2 == 0 {
+                continue;
+            }
+            if n % factor == 0 && factor < num {
+                // Definitely not a prime
+                break;
+            }
+            if n % factor == 0 {
+                is_prime = true;
+            }
+        }
+        if is_prime {
+            return n;
+        };
+    }
     0
 }
